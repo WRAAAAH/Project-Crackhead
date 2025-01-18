e4f11f0cff986dc74eafd938c1326705fc742fff
WRAAAAH <sowa.bruno@outlook.com>
2025-01-18 15:16:36 +0100

    Slight fixes

--- controller/urls.py
+++ controller/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('account/', include('allauth.urls')),
+    path('login/', views.login, name='login'),
 ]
--- controller/views.py
+++ controller/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'controller/index.html')
+
+def login(request):
+    return render(request, 'account/login.html')
